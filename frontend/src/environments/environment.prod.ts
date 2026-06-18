be4627e1fd9253566cf19cf888b81f08f9b2d415
Kaviya V <121804254+7-Kaviya@users.noreply.github.com>
2026-06-18 15:05:32 +0530

    Added environment for deployment

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  apiUrl: 'https://nexus-platform-backend-production.up.railway.app/', 
+  apiDelay: 0,      // No delay needed for your live server
+};
